42d2c6f9485e4e0597b94fb2ff82204cd088e73b
Mzes350 <goliathxemo@gmail.com>
2025-06-06 22:59:47 +0300

    second commit

--- REVENUE-FROM-CUSTOMERS.C
+++ REVENUE-FROM-CUSTOMERS.C
@@ -3,15 +3,15 @@
 int main ()
 {
 
+ int customer1 = 200;
+    int customer2 = 500;
+    int customer3 = 300;
+    int total_revenue;
 
+    // Calculate total revenue
+    total_revenue = customer1 + customer2 + customer3;
 
+    printf("Total revenue: UGX %d\n", total_revenue);
 
-
-
-
-
-
-
-
-    return 0
+    return 0;
 }
