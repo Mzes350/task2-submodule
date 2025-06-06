3f47caa73c86f13aa9d2842a525f2322671bb715
Mzes350 <goliathxemo@gmail.com>
2025-06-06 21:45:28 +0300

    second commit

--- average_sales.c
+++ average_sales.c
@@ -2,15 +2,15 @@
 
 int main ()
 {
+    float total_sales = 60000.0;
+    int days = 30;
+    float average_sales;
+
+    // Calculate the average sales
+    average_sales = total_sales / days;
 
 
-
-
-
-
-
-
-
+    printf("Average sales per day: UGX %.2f\n", average_sales);
 
 
     return 0;
