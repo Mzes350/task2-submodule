54f1c2518480a5ce93d831579220c40b84e72df9
Mzes350 <goliathxemo@gmail.com>
2025-06-06 22:18:33 +0300

    Second commit

--- Depreciation.c
+++ Depreciation.c
@@ -3,15 +3,16 @@
 int main ()
 {
 
+    float asset_cost = 5000.0;
+    float residual_value = 500.0;
+    int useful_life = 5;
+    float depreciation;
+
+    // Calculate depreciation
+    depreciation = (asset_cost - residual_value) / useful_life;
 
 
-
-
-
-
-
-
-
+    printf("Annual depreciation: UGX %.2f\n", depreciation);
 
     return 0;
 }
