4564cf59f25a4bf779fff484adfae8f7645f79e5
Mzes350 <goliathxemo@gmail.com>
2025-06-06 22:42:21 +0300

    Second commit

--- annualSalaryCalculation.c
+++ annualSalaryCalculation.c
@@ -2,16 +2,16 @@
 
 int main()
 {
+    float hourly_wage = 30.0;
+    int hours_per_week = 40;
+    int weeks_per_year = 52;
+    float annual_salary;
 
 
+    annual_salary = hourly_wage * hours_per_week * weeks_per_year;
 
 
-
-
-
-
-
-
+    printf("Annual salary: UGX %.2f\n", annual_salary);
 
     return 0;
 }
