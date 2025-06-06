af07011ee8ca8a24138275b591cb2832b55c6625
Mzes350 <goliathxemo@gmail.com>
2025-06-06 22:05:34 +0300

    second commit

--- commision_Calculation.c
+++ commision_Calculation.c
@@ -2,14 +2,15 @@
 
 int main()
 {
+float sales_volume = 50000.0;
+    float commission_rate = 0.05; // 5%
+    float commission;
 
+    // Calculate the commission
+    commission = sales_volume * commission_rate;
 
-
-
-
-
-
-
+    // Output the result
+    printf("Commission: UGX %.2f\n", commission);
 
 
     return 0;
